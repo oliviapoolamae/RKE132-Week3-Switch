cdd43687b480f0563ddf2d662548ee95e49644c9
oliviapoolamae <olivia.poolamae@tktk.ee>
2024-09-08 19:58:17 +0300

    add Week3IfAndSwitch code

---
+++ OddOrEven/Program.cs
@@ -0,0 +1,17 @@
+﻿
+
+Console.WriteLine("Enter a number:");
+int userNum = Convert.ToInt32(Console.ReadLine());
+
+//kas number on paaris või paaritu
+
+int result = userNum % 2;
+
+if (result != 0)
+{
+    Console.WriteLine("User number is odd.");
+}
+else
+{
+    Console.WriteLine("user number is even.");
+}
